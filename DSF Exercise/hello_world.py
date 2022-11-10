899df0677754b120023761e0c9307dfac2b3547c
Yannik Biebert <yannik.biebert@gmai.com>
2022-11-10 11:15:34 +0100

    This is a test in a new foulder

---
+++ DSF Exercise/hello_world.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# initialize list of lists
+data = [['tom', 10], ['nick', 15], ['juli', 14]]
+  
+# Create the pandas DataFrame
+df = pd.DataFrame(data, columns=['Name', 'Age'])
+  
+# print dataframe.
+print(df)
